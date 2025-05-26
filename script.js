a6e95e8e55f4f6cd5dda1fe88cabe01c9ff69879
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-05-26 14:44:45 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputItem = document.getElementById("input-item")
+
+const botaoAdicionar = document.getElementById('adicionar-item');
+let contador = 0;
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === "") {
+          alert("Por favor, insira um item!");
+          return
+    }
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.type = "checkbox";
+    inputCheckBox.id = "checkbox-" + contador++;
+})
